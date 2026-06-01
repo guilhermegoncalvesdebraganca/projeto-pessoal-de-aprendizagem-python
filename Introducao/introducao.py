7497a75e498610bd2f6b839a12ce22cabc648d10
Guilerme Goncalves <guibragn@gmail.com>
2026-06-01 11:52:36 -0300

    Add files via upload

---
+++ Introducao/introducao.py
@@ -0,0 +1,11 @@
+print("Hello Python!")
+print("Hello Word!"),
+
+#Comentários
+# Utilizar "#" para comentários curtos.
+
+"""
+Este é um comentário de múltiplas linhas.
+"""
+
+#TODO: utilizado para comentar futuras tarefas.
